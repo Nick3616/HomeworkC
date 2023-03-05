f0976447e1deda0e3f9facabd0e368373d536642
Stas <www.stas1996@mail.ru>
2023-03-05 20:17:27 +0300

    Задача 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-ие число: ");
+int b = int.Parse(Console.ReadLine()!);
+int max = n;
+if (max < m)
+max = m;    
+if (max < b)
+max = b;
+Console.WriteLine($"Максимальное число: {max}");
