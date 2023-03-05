178b4434ba5cd90a3bb98abb8106cb4706f734d4
Stas <www.stas1996@mail.ru>
2023-03-05 19:46:49 +0300

    задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n > m)
+    Console.WriteLine($"max = {n} min = {m}");
+else
+    Console.WriteLine($"max = {m} min = {n}");
